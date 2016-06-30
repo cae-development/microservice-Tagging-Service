4e9459538e94ff96653cc3d4d0e7a2f4ddf7ef16
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-30 21:21:55 +0200

    Code regeneration/Model synchronization

---
+++ db/Tagging_Service_create_tables.sql
@@ -0,0 +1,15 @@
+--
+-- Database Schema:  tags 
+-- Automatically generated sql script for the service Tagging Service, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table comments.
+--
+CREATE TABLE `tags`.`comments` (
+  imgId int ,
+  id int ,
+  comment text ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+);
+
