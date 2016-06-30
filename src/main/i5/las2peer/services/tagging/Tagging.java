56aaa66c2b61266100f5b8024d953810e6dc5e61
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-30 21:37:10 +0200

    Http Response[comments] edited by Thomas Winkler

--- src/main/i5/las2peer/services/tagging/Tagging.java
+++ src/main/i5/las2peer/services/tagging/Tagging.java
@@ -115,7 +115,7 @@ public class Tagging extends Service {
             commentJson.put("text",result.getString("comment"));
             array.add(commentJson);
         }
-        commentsJson.put("comments", array);aaaaa
+        commentsJson.put("comments", array);aaaa
 
         HttpResponse commentsResult = new HttpResponse(commentsJson.toJSONString(), HttpURLConnection.HTTP_OK);
         return commentsResult;
